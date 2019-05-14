526d41552f9106fbaa7e7bf8c0cf921602df42a3
antonio <afj014@inlumine.ual.es>
2019-05-14 16:16:36 +0200

    Comentar main

--- cmd/dra/main.go
+++ cmd/dra/main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"github.com/antonioofdz/personalprojectdra/pkg/database"
 	"github.com/antonioofdz/personalprojectdra/pkg/handlers"
 )
 
-func init() {
-	if err := database.InitDB(); err != nil {
-		panic(err)
-	}
-}
-
 func main() {
+	/*if err := database.InitDB(); err != nil {
+		panic(err)
+	}*/
+
 	handlers.LoadRoutes()
 }
